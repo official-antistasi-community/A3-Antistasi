7a4ec9421a4fba0eb84b50921fffdebc3421b003
ante <49555217+ante185@users.noreply.github.com>
2024-11-18 23:08:20 +0100

    initial

---
+++ A3A/addons/Zeus/CfgVehicles.hpp
@@ -0,0 +1,144 @@
+class CfgVehicles
+{
+    class I_G_Survivor_F;
+    class a3a_unit_reb_unarmed : I_G_Survivor_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_F;
+    class a3a_unit_reb : I_G_Soldier_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_medic_F;
+    class a3a_unit_reb_medic : I_G_medic_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Sharpshooter_F;
+    class a3a_unit_reb_sniper : I_G_Sharpshooter_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_M_F;
+    class a3a_unit_reb_marksman : I_G_Soldier_M_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_LAT_F;
+    class a3a_unit_reb_lat : I_G_Soldier_LAT_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_AR_F;
+    class a3a_unit_reb_mg : I_G_Soldier_AR_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_exp_F;
+    class a3a_unit_reb_exp : I_G_Soldier_exp_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_GL_F;
+    class a3a_unit_reb_gl : I_G_Soldier_GL_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_Soldier_SL_F;
+    class a3a_unit_reb_sl : I_G_Soldier_SL_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_G_engineer_F;
+    class a3a_unit_reb_eng : I_G_engineer_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_Soldier_AT_F;
+    class a3a_unit_reb_at : I_Soldier_AT_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class I_Soldier_AA_F;
+    class a3a_unit_reb_aa : I_Soldier_AA_F {
+        class EventHandlers;
+        editorCategory = "a3a_rebels";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+
+    // Base side types
+
+    class B_G_Soldier_F;
+    class a3a_unit_west : B_G_Soldier_F {
+        class EventHandlers;
+        editorCategory = "a3a_occupants";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class O_G_Soldier_F;
+    class a3a_unit_east : O_G_Soldier_F {
+        class EventHandlers;
+        editorCategory = "a3a_invaders";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    class C_Man_1;
+    class a3a_unit_civ : C_Man_1 {
+        class EventHandlers;
+        editorCategory = "a3a_civilian";
+        editorSubcategory = "a3a_men_internal";
+        scopeCurator = 0;
+    };
+
+    //Occupant Military
+    
+    class a3a_unit_west_rifleman : a3a_unit_west {
+        class EventHandlers : EventHandlers {
+            init = "#include '\x\A3A\addons\Zeus\initUnitLoadout.sqf';"
+        };
+		displayName = "Rifleman";
+        editorSubcategory = "a3a_military";
+        scopeCurator = 2;
+    };
+};
---
+++ A3A/addons/Zeus/config.cpp
@@ -0,0 +1,60 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3A_Events", "A3A_core", "A3A_config_fixes"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+class CfgEditorCategories
+{
+	class a3a_occupants
+	{
+		displayName = "A3A Occupants";
+	};
+	class a3a_invaders
+	{
+		displayName = "A3A Invaders";
+	};
+	class a3a_rebels
+	{
+		displayName = "A3A Rebels";
+	};
+	class a3a_civilian
+	{
+		displayName = "A3A Civilian";
+	};
+};
+class CfgEditorSubcategories
+{
+	class a3a_men_internal
+	{
+		displayName = "Men (Internal A3A use)";
+	};
+	class a3a_military
+	{
+		displayName = "Men (Military)";
+	};
+	class a3a_militia
+	{
+		displayName = "Men (Militia)";
+	};
+	class a3a_specops
+	{
+		displayName = "Men (Spec Ops)";
+	};
+	class a3a_other
+	{
+		displayName = "Men (Other)";
+	};
+};
+
+#include "CfgVehicles.hpp"
