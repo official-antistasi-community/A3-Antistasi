b479452882883c6779cc3d4308ec4a1920071e70
SSGEnfisk <133915772+SSGEnfisk@users.noreply.github.com>
2025-02-25 10:43:42 +0100

    Add files via upload

--- A3A/addons/maps/Antistasi_stubbhult.stubbhult/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_stubbhult.stubbhult/mapInfo.hpp
@@ -1,6 +1,38 @@
 class stubbhult {
-	population[] = {}; //automated data is fine and needs no adaptation
-	disabledTowns[] = {};
+	population[] = {
+		{"gussbo",28},
+		{"aker",73},
+		{"bockshult",45},
+		{"ekenas",250},
+		{"bexet",55},
+		{"hylte",100},
+		{"gullanabba",120},
+		{"balshult",130},
+		{"hallaboke",51},
+		{"saraboke",25},
+		{"hokhult",35},
+		{"boalt",170},
+		{"lojenas",78},
+		{"sodra_bokeberg",30},
+		{"norra_bokeberg",20},
+		{"rangalsbo",65},
+		{"rangelsnas",45},
+		{"ekenaso",10},
+		{"krakshult",25},
+		{"kalvhult",45},
+		{"gnosjo",65},
+		{"hjalmshult",20},
+		{"sofieslatt",25},
+		{"mjalahult",65},
+		{"vallshult",25},
+		{"unnaryd",1},
+		{"yttre_mjala",55},
+		{"dohult",20},
+		{"eldshult",45},
+		{"ostra_bokeberg",100},
+		{"langaryd",120}		
+	}; //automated data is fine and needs no adaptation
+	disabledTowns[] = {"backhasten","bohult","saxhult"};
 	antennas[] = {
 		{2914.1,6548.89,32.8618},{2431.71,9784.5,14.8483},{7321.8,10270.2,25.024},{9151.14,11213.6,8.3},{10272.4,9230.21,11.9453},{5638.5,7275.06,1.33},{5522.2,4569.69,31.5929},{4572.35,731.819,35.3782},{8090.82,3098.02,21}
 	};
