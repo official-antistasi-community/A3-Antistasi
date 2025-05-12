6e6a08d8eacab5f667ede45c2a65bbe99455adba
ante <49555217+ante185@users.noreply.github.com>
2025-05-13 00:14:33 +0200

    Update CfgAmmo.hpp

--- A3A/addons/config_fixes/IFA/CfgAmmo.hpp
+++ A3A/addons/config_fixes/IFA/CfgAmmo.hpp
@@ -1,11 +1,23 @@
 // IFA - CfgAmmo.hpp
 
-class CfgAmmo {
 
+class CfgAmmo {
+    class LIB_Bullet_Plane_base;
+    class LIB_Bullet_AP_base;
     class LIB_Bullet_Vehicle_base;
+
     class LIB_B_127x99_Ball : LIB_Bullet_Vehicle_base {
         caliber = 1.43;
         hit = 20;
+    };
+    class LIB_B_37mm_61k_AP : LIB_Bullet_AP_base {
+        ace_vehicle_damage_incendiary = 0.5;
+        caliber = 3.0;
+    };
+    class LIB_B_37mm_AP : LIB_Bullet_Plane_base {
+        ace_vehicle_damage_incendiary = 0.5;
+        indirectHit = 4;
+        indirectHitRange = 3;
     };
     // Buffs to bring mortar effectiveness against unarmoured somewhere near vanilla against armoured
     class Sh_82mm_AMOS;
