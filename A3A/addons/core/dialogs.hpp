493b08129a5576d408fce6788af01977817625eb
jaj22 <jaj22x@gmail.com>
2025-05-16 22:46:12 +0100

    Change UI setting to a per-user toggle in game options (#3600)

--- A3A/addons/core/dialogs.hpp
+++ A3A/addons/core/dialogs.hpp
@@ -874,13 +874,13 @@ class game_options 		{
 		class 8slots_L2: A3A_core_BattleMenuRedButton
 		{
 			idc = -1;
-			text = "";	//$STR_antistasi_dialogs_maps_ai_limiter;
+			text = $STR_antistasi_dialogs_maps_toggle_ui;
 			x = 0.272481 * safezoneW + safezoneX;
 			y = 0.415981 * safezoneH + safezoneY;
 			w = 0.175015 * safezoneW;
 			h = 0.0560125 * safezoneH;
-			tooltip = "";	//$STR_antistasi_dialogs_maps_ai_limiter_tooltip;
-			//action = "if (call BIS_fnc_admin > 0 || isServer) then {closeDialog 0; nul = createDialog ""fps_limiter""} else {[""AI Limiter"", ""Only admins have access to this function.""] call A3A_fnc_customHint;};";
+			tooltip = $STR_antistasi_dialogs_maps_toggle_ui_tooltip;
+			action = "A3A_GUIDevPreview = !A3A_GUIDevPreview; profileNamespace setVariable ['AntistasiUseNewUI', A3A_GUIDevPreview]; ['UI toggle', format ['Switching to the %1 UI', ['old', 'new'] select A3A_GUIDevPreview]] call A3A_fnc_customHint;";
 		};
 		class 8slots_R2: A3A_core_BattleMenuRedButton
 		{
