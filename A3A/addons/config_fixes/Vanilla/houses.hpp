f742217253e5ea1d7015eef1a102facc7ad00e54
John Jordan <jaj22x@gmail.com>
2025-07-06 05:41:15 +0100

    Malden and Tanoa police stations

--- A3A/addons/config_fixes/Vanilla/houses.hpp
+++ A3A/addons/config_fixes/Vanilla/houses.hpp
@@ -10,6 +10,18 @@ class Land_i_House_Big_01_V1_F : House_F {
     };
 };
 
+// Malden houses: As above but different base class for some reason
+class i_House_Big_01_b_base_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{5.22,2.82,-1.59},-90},
+        {"Banner_01_F",{-4.82,-6.00,2.00},90},
+        {"OfficeTable_01_old_F",{2.30,-6.68,0.85},-180},
+        {"Land_OfficeChair_01_F",{2.73,-5.44,0.85},70},
+        {"Land_OfficeCabinet_01_F",{4.30,-3.97,0.85},-270},
+        {"Box_NATO_Wps_F",{1.84,5.17,0.85},-90}
+    };
+};
+
 class Land_i_House_Big_02_V1_F : House_F {
     A3A_policeStationObjects[] = {
         {"Banner_01_F",{3.42285,-4.49707,-1.28631},0},
@@ -17,5 +29,102 @@ class Land_i_House_Big_02_V1_F : House_F {
         {"Land_OfficeChair_01_F",{1.58594,1.44824,0.781626},38.6148},
         {"Land_OfficeCabinet_02_F",{4.41895,1.04395,0.781635},89.9963},
         {"Box_NATO_Wps_F",{-3.81,2.31,0.78},270}
+    };
+};
+
+// Malden houses: As above but different base class for some reason
+class i_House_Big_02_b_base_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{3.42285,-4.49707,-1.28631},0},
+        {"OfficeTable_01_old_F",{0.981445,0.0810547,0.781624},179.999},
+        {"Land_OfficeChair_01_F",{1.58594,1.44824,0.781626},38.6148},
+        {"Land_OfficeCabinet_02_F",{4.41895,1.04395,0.781635},89.9963},
+        {"Box_NATO_Wps_F",{-3.81,2.31,0.78},270}
+    };
+};
+
+// Tanoa orange building
+class Land_House_Big_04_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{-5.84814,-5.74072,-1.79159},0},
+        {"Banner_01_F",{11.1899,-0.224365,1.35066},270},
+        {"Land_OfficeCabinet_02_F",{3.78955,-1.42261,-2.94956},0},
+        {"OfficeTable_01_old_F",{4.47754,-3.07422,-2.94956},90},
+        {"Land_OfficeChair_01_F",{2.96777,-2.69922,-2.94958},330},
+        {"Box_NATO_Wps_F",{3.4541,2.61694,0.300415},270}
+    };
+};
+
+//Tanoa blue/white building
+class Land_House_Big_03_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{11.6694,0.500488,-1.69798},-90},
+        {"Banner_01_F",{3.0542,-3.771,1.39992},0},
+        {"Banner_01_F",{1.36475,4.29248,-1.87074},180},
+        {"OfficeTable_01_old_F",{3.99219,-3.07617,-0.0915375},180},
+        {"Land_OfficeCabinet_02_F",{1.96436,-0.850098,-0.0915318},-90},
+        {"Land_OfficeChair_01_F",{3.50879,-1.56006,-0.0915318},-39.581},
+        {"Box_NATO_Wps_F",{2.91455,2.67334,-0.0915375},-90}
+    };
+};
+
+// Tanoa single floor
+class Land_House_Big_01_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{4.4458,6.34668,0.251594},180},
+        {"Banner_01_F",{7.1543,-1.72363,0.21869},0},
+        {"OfficeTable_01_old_F",{2.61035,5.61279,-1.01386},0},
+        {"Land_OfficeCabinet_02_F",{-0.107422,4.58008,-1.01382},270},
+        {"Land_OfficeChair_01_F",{1.83643,4.1123,-1.01383},211},
+        {"Box_NATO_Wps_F",{6.71338,-0.689453,-1.01387},0}
+    };
+};
+
+// Tanoa single shop
+class Land_Shop_Town_01_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{-0.0219727,-7.4375,-0.149916},0},
+        {"Banner_01_F",{-3.34717,6.50537,-1.79974},180},
+        {"Land_OfficeCabinet_02_F",{-3.30273,2.21777,-3.2461},360},
+        {"OfficeTable_01_old_F",{-4.23047,0.566406,-3.24611},270},
+        {"Land_OfficeChair_01_F",{-2.65527,0.553711,-3.2461},42},
+        {"Box_NATO_Wps_F",{-3.94531,-3.51172,-3.24611},90}
+    };
+};
+
+// Tanoa double shop
+class Land_Shop_Town_03_F : House_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{7.10596,-5.50732,-1.81075},-90},
+        {"Banner_01_F",{-6.87988,-4.9834,-1.58109},90},
+        {"Banner_01_F",{-5.46729,8.55322,-1.94165},-180},
+        {"Box_NATO_Wps_F",{3.01025,2.07861,-3.36066},-180},
+        {"OfficeTable_01_old_F",{0.825195,0.389648,-3.36066},0},
+        {"Land_OfficeCabinet_02_F",{-0.955078,-0.232422,-3.36065},-90},
+        {"Land_OfficeChair_01_F",{1.2793,-1.25391,-3.36066},151.237}
+    };
+};
+
+// Tanoa two-room shack
+class House_Small_F;
+class Land_House_Small_01_F : House_Small_F {
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{2.05371,3.97021,0.273073},180},
+        {"Banner_01_F",{5.59473,-4.07715,0.223607},0},
+        {"OfficeTable_01_old_F",{-6.01416,0.95166,-0.701801},270},
+        {"Land_OfficeCabinet_02_F",{-4.21924,3.58789,-0.701774},0},
+        {"Land_OfficeChair_01_F",{-4.25146,1.62793,-0.701799},39.2858},
+        {"Box_NATO_Wps_F",{-0.333008,3.01074,-0.701802},0}
+    };
+};
+
+// Tanoa two-room brick shack
+class Land_House_Small_02_F : House_Small_F {
+    A3A_policeStationObjects[] = {
+        ["Banner_01_F",[1.1499,0.150879,0.420892],270],
+        ["Banner_01_F",[-4.94482,0.259766,0.345404],90],
+        ["OfficeTable_01_old_F",[-3.02734,5.78784,-0.71664],360],
+        ["Land_OfficeCabinet_02_F",[-4.2251,4.73022,-0.716618],270],
+        ["Box_NATO_Wps_F",[-3.64893,-5.50439,-0.716641],0]
     };
 };
