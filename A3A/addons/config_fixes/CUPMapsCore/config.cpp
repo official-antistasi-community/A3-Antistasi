bcda035e9e5f0489fcd9c82f2773a37dc1ee5ff8
John Jordan <jaj22x@gmail.com>
2025-07-10 19:15:48 +0100

    Add police stations for original Chernarus maps

---
+++ A3A/addons/config_fixes/CUPMapsCore/config.cpp
@@ -0,0 +1,24 @@
+//CUP maps core - config.cpp
+
+#include "..\script_component.hpp"
+
+class CfgPatches 
+{
+    class PATCHNAME(CUPMapsCore)
+    {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"CUP_Core"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        skipWhenMissingDependencies = 1;
+        VERSION_CONFIG;
+    };
+};
+
+class CfgVehicles {
+    #include "houses.hpp"
+};
---
+++ A3A/addons/config_fixes/CUPMapsCore/houses.hpp
@@ -0,0 +1,75 @@
+
+// big red buildings
+class Land_HouseV2_04;
+class Land_HouseV2_04_interier : Land_HouseV2_04
+{
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{2.69043,-7.03931,-4.53918},0},
+        {"Banner_01_F",{3.49219,7.89832,-4.63599},180},
+        {"OfficeTable_01_old_F",{3.27002,7.12366,-5.74087},0},
+        {"Land_ChairWood_F",{2.97607,6.58521,-5.74105},180},
+        {"Box_NATO_Wps_F",{-1.4126,2.86682,-5.74223},0}
+    };
+};
+
+class Land_HouseV2_02;
+class Land_HouseV2_02_Interier : Land_HouseV2_02
+{
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{-3.01953,7.65247,-4.25836},180},
+        {"Banner_01_F",{3.0249,-7.70349,-4.38084},0},
+        {"OfficeTable_01_old_F",{5.48145,-2.11572,-5.53312},180},
+        {"Land_ChairWood_F",{5.6665,-1.5675,-5.53312},0},
+        {"Box_NATO_Wps_F",{-8.19971,-1.81995,-5.53312},0}
+    };
+};
+
+// pub/restaurant
+class House;
+class Land_A_Pub_01 : House
+{
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{-1.2168,2.88391,-4.28544},90},
+        {"Banner_01_F",{6.271,-5.90845,-2.04558},315},
+        {"Box_NATO_Wps_F",{-7.38086,-3.73157,-1.76366},90},
+        {"OfficeTable_01_old_F",{3.75635,2.07666,-1.76366},180},
+        {"Land_ChairWood_F",{4.12012,2.4585,-1.76366},0}
+    };
+};
+
+// Fire station
+class Land_a_stationhouse : House
+{
+    A3A_policeStationObjects[] = {
+    	{"Banner_01_F",{-8.91162,-8.37109,-7.93235},0},
+        {"Banner_01_F",{-5.55029,9.67236,-8.36535},180},
+        {"Box_NATO_Wps_F",{-17.1279,3.42493,-6.7813},270},
+        {"OfficeTable_01_old_F",{-0.950684,2.69263,-6.7813},90},
+        {"Land_OfficeCabinet_02_F",{-0.876953,0.891968,-6.78129},90},
+        {"Land_ChairWood_F",{-1.4458,2.86865,-6.7813},270}
+    };
+};
+
+// shitty shack
+class Land_HouseV_1I4 : House
+{
+    A3A_policeStationObjects[] = {
+    	{"Banner_01_F",{1.36621,3.96838,-1.7723},270},
+        {"Banner_01_F",{-5.13086,-3.17371,-1.7163},90},
+        {"OfficeTable_01_old_F",{-4.47852,-2.97937,-2.74871},270},
+        {"Box_NATO_Wps_F",{0.674805,-2.75806,-2.74883},90}
+    };
+};
+
+// guardhouse. Furnished?
+
+class Land_Mil_Guardhouse : House
+{
+    A3A_policeStationObjects[] = {
+        {"Banner_01_F",{-1.91846,2.80212,-0.61635},90},
+        {"Banner_01_F",{5.44775,-0.69751,-0.50935},270},
+        {"Box_NATO_Wps_F",{4.39404,3.49805,-1.67183},0},
+        {"OfficeTable_01_old_F",{0.473633,3.71204,-1.67183},0},
+        {"Land_ChairWood_F",{0.242676,3.26221,-1.67179},180}
+    };
+};
