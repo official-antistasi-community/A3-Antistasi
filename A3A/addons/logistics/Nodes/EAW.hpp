de1e03953955be2f99fe4e0d43744ab04b3a90fc
ante <49555217+ante185@users.noreply.github.com>
2025-09-23 16:02:04 +0200

    restricted seat

--- A3A/addons/logistics/Nodes/EAW.hpp
+++ A3A/addons/logistics/Nodes/EAW.hpp
@@ -5,14 +5,22 @@ class EAW_Cars_EAW_Dodge1936_Pickup : TRIPLES(ADDON,Nodes,Base)
         class Node1
         {
             offset[] = {0,-0.6,-0.5};
+            seats[] = {0,7};
         };
         class Node2
         {
             offset[] = {0,-1.4,-0.5};
+            seats[] = {1,2,5,6};
         };
         class Node3
         {
             offset[] = {0,-2.2,-0.5};
+            seats[] = {3,4};
+        };
+        class Node4
+        {
+            offset[] = {0,-3.0,-0.5};
+            seats[] = {8,9,10};
         };
     };
 };
@@ -24,15 +32,23 @@ class EAW_Cars2_EAW_T94_Truck : TRIPLES(ADDON,Nodes,Base)
     {
         class Node1
         {
-            offset[] = {0,0,-0.5};
+            offset[] = {0,0.15,-0.5};
+            seats[] = {0,7};
         };
         class Node2
         {
-            offset[] = {0,-0.8,-0.5};
+            offset[] = {0,-0.65,-0.5};
+            seats[] = {1,2,5,6};
         };
         class Node3
         {
-            offset[] = {0,-1.6,-0.5};
+            offset[] = {0,-1.45,-0.5};
+            seats[] = {3,4};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.25,-0.5};
+            seats[] = {8,9,10};
         };
     };
 };
