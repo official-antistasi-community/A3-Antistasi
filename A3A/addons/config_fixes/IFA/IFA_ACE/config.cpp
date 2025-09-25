177524ddcb4fcac6409f13fb2e799aa240a997ec
ante <49555217+ante185@users.noreply.github.com>
2025-09-25 02:42:36 +0200

    Update config.cpp

--- A3A/addons/config_fixes/IFA/IFA_ACE/config.cpp
+++ A3A/addons/config_fixes/IFA/IFA_ACE/config.cpp
@@ -5,11 +5,18 @@
 
 class CfgPatches 
 {
-    class A3A_config_fixes_IFA;
-    class PATCHNAME(IFA_ACE) : A3A_config_fixes_IFA
+    class PATCHNAME(IFA_ACE)
     {
         name = COMPONENT_NAME;
+        units[] = {"a3a_hmg_02_high", "a3a_hmg_02_high_weapon","a3a_LIB_Willys_MB_M1919","a3a_lib_Zis6_BOX","a3a_lib_PzKpfwIV_noShield"};
+        weapons[] = {"a3a_lib_M712","a3a_lib_AVT_40","a3a_lib_M2_Carbine"};
+        requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"A3A_config_fixes_IFA","IFA3_COMP_ACE_main","IFA3_COMP_ACE_mortar"};
+        skipWhenMissingDependencies = 1;
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
     };
 };
 
