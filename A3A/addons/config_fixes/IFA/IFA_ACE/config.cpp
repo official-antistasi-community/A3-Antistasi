554d7b63831eccd45699e4f5fa63a398ca0be271
ante185 <49555217+ante185@users.noreply.github.com>
2025-10-15 21:18:42 +0200

    IFA Config Fix, Supply/Box truck and IFA ammunition adjustments (#3594)
    - Made a3a_lib_Zis6_BOX not an ACE repair vehicle
    - Switched the .50 used for IFA to use IFA .50 weapon

--- A3A/addons/config_fixes/IFA/IFA_ACE/config.cpp
+++ A3A/addons/config_fixes/IFA/IFA_ACE/config.cpp
@@ -1,17 +1,17 @@
 //IFA - config.cpp
 
-#include "..\script_component.hpp"
+#include "..\..\script_component.hpp"
 
 
 class CfgPatches 
 {
-    class PATCHNAME(IFA)
+    class PATCHNAME(IFA_ACE)
     {
         name = COMPONENT_NAME;
         units[] = {"a3a_hmg_02_high", "a3a_hmg_02_high_weapon","a3a_LIB_Willys_MB_M1919","a3a_lib_Zis6_BOX","a3a_lib_PzKpfwIV_noShield"};
         weapons[] = {"a3a_lib_M712","a3a_lib_AVT_40","a3a_lib_M2_Carbine"};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"IFA3_Core","WW2_Core_c_IF_ZZZ_LastLoaded_c"};
+        requiredAddons[] = {"A3A_config_fixes_IFA","IFA3_COMP_ACE_main","IFA3_COMP_ACE_mortar"};
         skipWhenMissingDependencies = 1;
         author = AUTHOR;
         authors[] = { AUTHORS };
@@ -20,7 +20,6 @@ class CfgPatches
     };
 };
 
-// Uncomment when needed
-#include "CfgVehicles.hpp"
-#include "CfgWeapons.hpp"
-#include "CfgAmmo.hpp"
+#include "..\CfgVehicles.hpp"
+#include "..\CfgWeapons.hpp"
+#include "..\CfgAmmo.hpp"
