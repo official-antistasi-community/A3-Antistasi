47eced330a2d97ad4f491187f62fd7b433e663bf
skillless402 <Skillless402@gmail.com>
2025-10-19 15:34:09 -0400

    space remover
    removed spaces

--- A3A/addons/config_fixes/RHS/RHS_Tank_Ammo.hpp
+++ A3A/addons/config_fixes/RHS/RHS_Tank_Ammo.hpp
@@ -1,9 +1,9 @@
 class rhsusf_m1a1tank_base;
-class rhsusf_m1a1aimd_usarmy : rhsusf_m1a1tank_base
+class rhsusf_m1a1aimd_usarmy:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1aimd_usarmy_base: rhsusf_m1a1aimd_usarmy
+class a3a_rhsusf_m1a1aimd_usarmy_base:rhsusf_m1a1aimd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
@@ -23,17 +23,17 @@ class a3a_rhsusf_m1a1aimd_usarmy_f:a3a_rhsusf_m1a1aimd_usarmy_base
   };
 };
 
-class rhsusf_m1a1aimwd_usarmy: rhsusf_m1a1tank_base
+class rhsusf_m1a1aimwd_usarmy:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1aimwd_usarmy_base: rhsusf_m1a1aimwd_usarmy
+class a3a_rhsusf_m1a1aimwd_usarmy_base:rhsusf_m1a1aimwd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1aimwd_usarmy_f:a3a_rhsusf_m1a1aimwd_usarmy_base
 {
@@ -47,17 +47,17 @@ class a3a_rhsusf_m1a1aimwd_usarmy_f:a3a_rhsusf_m1a1aimwd_usarmy_base
   };
 };
 
-class rhsusf_m1a1aim_tuski_wd: rhsusf_m1a1tank_base
+class rhsusf_m1a1aim_tuski_wd:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1aim_tuski_wd_base: rhsusf_m1a1aim_tuski_wd
+class a3a_rhsusf_m1a1aim_tuski_wd_base:rhsusf_m1a1aim_tuski_wd
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1aim_tuski_wd_f:a3a_rhsusf_m1a1aim_tuski_wd_base
 {
@@ -72,17 +72,17 @@ class a3a_rhsusf_m1a1aim_tuski_wd_f:a3a_rhsusf_m1a1aim_tuski_wd_base
 };
 
 class rhsusf_m1a2tank_base;
-class rhsusf_m1a2sep1d_usarmy: rhsusf_m1a2tank_base
+class rhsusf_m1a2sep1d_usarmy:rhsusf_m1a2tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1d_usarmy_base: rhsusf_m1a2sep1d_usarmy
+class a3a_rhsusf_m1a2sep1d_usarmy_base:rhsusf_m1a2sep1d_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1d_usarmy:a3a_rhsusf_m1a2sep1d_usarmy_base
 {
@@ -96,17 +96,17 @@ class a3a_rhsusf_m1a2sep1d_usarmy:a3a_rhsusf_m1a2sep1d_usarmy_base
   };
 };
 
-class rhsusf_m1a2sep1tuskid_usarmy: rhsusf_m1a2tank_base
+class rhsusf_m1a2sep1tuskid_usarmy:rhsusf_m1a2tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1tuskid_usarmy_base: rhsusf_m1a2sep1tuskid_usarmy
+class a3a_rhsusf_m1a2sep1tuskid_usarmy_base:rhsusf_m1a2sep1tuskid_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1tuskid_usarmy_f:a3a_rhsusf_m1a2sep1tuskid_usarmy_base
 {
@@ -120,17 +120,17 @@ class a3a_rhsusf_m1a2sep1tuskid_usarmy_f:a3a_rhsusf_m1a2sep1tuskid_usarmy_base
   };
 };
 
-class rhsusf_m1a2sep1tuskiiwd_usarmy: rhsusf_m1a2sep1tuskid_usarmy
+class rhsusf_m1a2sep1tuskiiwd_usarmy:rhsusf_m1a2sep1tuskid_usarmy
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_base: rhsusf_m1a2sep1tuskiiwd_usarmy
+class a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_base:rhsusf_m1a2sep1tuskiiwd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_f:a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_base
 {
@@ -144,17 +144,17 @@ class a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_f:a3a_rhsusf_m1a2sep1tuskiiwd_usarmy_ba
   };
 };
 
-class rhsusf_m1a2sep1tuskiid_usarmy: rhsusf_m1a2sep1tuskiiwd_usarmy
+class rhsusf_m1a2sep1tuskiid_usarmy:rhsusf_m1a2sep1tuskiiwd_usarmy
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1tuskiid_usarmy_base: rhsusf_m1a2sep1tuskiid_usarmy
+class a3a_rhsusf_m1a2sep1tuskiid_usarmy_base:rhsusf_m1a2sep1tuskiid_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1tuskiid_usarmy_f:a3a_rhsusf_m1a2sep1tuskiid_usarmy_base
 {
@@ -168,17 +168,17 @@ class a3a_rhsusf_m1a2sep1tuskiid_usarmy_f:a3a_rhsusf_m1a2sep1tuskiid_usarmy_base
   };
 };
 
-class rhsusf_m1a2sep1tuskiwd_usarmy: rhsusf_m1a2sep1tuskid_usarmy
+class rhsusf_m1a2sep1tuskiwd_usarmy:rhsusf_m1a2sep1tuskid_usarmy
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1tuskiwd_usarmy_base: rhsusf_m1a2sep1tuskiwd_usarmy
+class a3a_rhsusf_m1a2sep1tuskiwd_usarmy_base:rhsusf_m1a2sep1tuskiwd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1tuskiwd_usarmy_f:a3a_rhsusf_m1a2sep1tuskiwd_usarmy_base
 {
@@ -193,17 +193,17 @@ class a3a_rhsusf_m1a2sep1tuskiwd_usarmy_f:a3a_rhsusf_m1a2sep1tuskiwd_usarmy_base
 };
 
 class rhsusf_m1a2sep2_base;
-class rhsusf_m1a2sep2d_usarmy: rhsusf_m1a2sep2_base
+class rhsusf_m1a2sep2d_usarmy:rhsusf_m1a2sep2_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep2d_usarmy_base: rhsusf_m1a2sep2d_usarmy
+class a3a_rhsusf_m1a2sep2d_usarmy_base:rhsusf_m1a2sep2d_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep2d_usarmy:a3a_rhsusf_m1a2sep2d_usarmy_base
 {
@@ -217,17 +217,17 @@ class a3a_rhsusf_m1a2sep2d_usarmy:a3a_rhsusf_m1a2sep2d_usarmy_base
   };
 };
 
-class rhsusf_m1a2sep2wd_usarmy: rhsusf_m1a2sep2_base
+class rhsusf_m1a2sep2wd_usarmy:rhsusf_m1a2sep2_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep2wd_usarmy_base: rhsusf_m1a2sep2wd_usarmy
+class a3a_rhsusf_m1a2sep2wd_usarmy_base:rhsusf_m1a2sep2wd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep2wd_usarmy:a3a_rhsusf_m1a2sep2wd_usarmy_base
 {
@@ -241,17 +241,17 @@ class a3a_rhsusf_m1a2sep2wd_usarmy:a3a_rhsusf_m1a2sep2wd_usarmy_base
   };
 };
 
-class rhsusf_m1a1aim_tuski_d: rhsusf_m1a1aim_tuski_wd
+class rhsusf_m1a1aim_tuski_d:rhsusf_m1a1aim_tuski_wd
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1aim_tuski_d_base: rhsusf_m1a1aim_tuski_d
+class a3a_rhsusf_m1a1aim_tuski_d_base:rhsusf_m1a1aim_tuski_d
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1aim_tuski_d:a3a_rhsusf_m1a1aim_tuski_d_base
 {
@@ -265,17 +265,17 @@ class a3a_rhsusf_m1a1aim_tuski_d:a3a_rhsusf_m1a1aim_tuski_d_base
   };
 };
 
-class rhsusf_m1a2sep1wd_usarmy: rhsusf_m1a1tank_base
+class rhsusf_m1a2sep1wd_usarmy:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a2sep1wd_usarmy_base: rhsusf_m1a2sep1wd_usarmy
+class a3a_rhsusf_m1a2sep1wd_usarmy_base:rhsusf_m1a2sep1wd_usarmy
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a2sep1wd_usarmy:a3a_rhsusf_m1a2sep1wd_usarmy_base
 {
@@ -289,17 +289,17 @@ class a3a_rhsusf_m1a2sep1wd_usarmy:a3a_rhsusf_m1a2sep1wd_usarmy_base
   };
 };
 
-class rhsusf_m1a1fep_wd: rhsusf_m1a1tank_base
+class rhsusf_m1a1fep_wd:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1fep_wd_base: rhsusf_m1a1fep_wd
+class a3a_rhsusf_m1a1fep_wd_base:rhsusf_m1a1fep_wd
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1fep_wd:a3a_rhsusf_m1a1fep_wd_base
 {
@@ -313,17 +313,17 @@ class a3a_rhsusf_m1a1fep_wd:a3a_rhsusf_m1a1fep_wd_base
   };
 };
 
-class rhsusf_m1a1fep_od: rhsusf_m1a1tank_base
+class rhsusf_m1a1fep_od:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1fep_od_base: rhsusf_m1a1fep_od
+class a3a_rhsusf_m1a1fep_od_base:rhsusf_m1a1fep_od
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1fep_od1:a3a_rhsusf_m1a1fep_od_base
 {
@@ -337,17 +337,17 @@ class a3a_rhsusf_m1a1fep_od1:a3a_rhsusf_m1a1fep_od_base
   };
 };
 
-class rhsusf_m1a1fep_d: rhsusf_m1a1tank_base
+class rhsusf_m1a1fep_d:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1fep_d_base: rhsusf_m1a1fep_d
+class a3a_rhsusf_m1a1fep_d_base:rhsusf_m1a1fep_d
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1fep_d:a3a_rhsusf_m1a1fep_d_base
 {
@@ -361,17 +361,17 @@ class a3a_rhsusf_m1a1fep_d:a3a_rhsusf_m1a1fep_d_base
   };
 };
 
-class rhsusf_m1a1hc_wd: rhsusf_m1a1tank_base
+class rhsusf_m1a1hc_wd:rhsusf_m1a1tank_base
 {
     class Turrets;
 };
-class a3a_rhsusf_m1a1hc_wd_base: rhsusf_m1a1hc_wd
+class a3a_rhsusf_m1a1hc_wd_base:rhsusf_m1a1hc_wd
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsusf_m1a1hc_wd:a3a_rhsusf_m1a1hc_wd_base
 {
@@ -388,17 +388,17 @@ class a3a_rhsusf_m1a1hc_wd:a3a_rhsusf_m1a1hc_wd_base
 //Russian tanks 
 
 class rhsgref_cdf_t72ba_tv;
-class rhsgref_cdf_b_t72ba_tv: rhsgref_cdf_t72ba_tv
+class rhsgref_cdf_b_t72ba_tv:rhsgref_cdf_t72ba_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t72ba_tv_base: rhsgref_cdf_b_t72ba_tv
+class a3a_rhsgref_cdf_b_t72ba_tv_base:rhsgref_cdf_b_t72ba_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t72ba_tv:a3a_rhsgref_cdf_b_t72ba_tv_base
 {
@@ -413,17 +413,17 @@ class a3a_rhsgref_cdf_b_t72ba_tv:a3a_rhsgref_cdf_b_t72ba_tv_base
 };
 
 class rhsgref_cdf_t72bb_tv;
-class rhsgref_cdf_b_t72bb_tv: rhsgref_cdf_t72bb_tv
+class rhsgref_cdf_b_t72bb_tv:rhsgref_cdf_t72bb_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t72bb_tv_base: rhsgref_cdf_b_t72bb_tv
+class a3a_rhsgref_cdf_b_t72bb_tv_base:rhsgref_cdf_b_t72bb_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t72bb_tv:a3a_rhsgref_cdf_b_t72bb_tv_base
 {
@@ -438,17 +438,17 @@ class a3a_rhsgref_cdf_b_t72bb_tv:a3a_rhsgref_cdf_b_t72bb_tv_base
 };
 
 class rhsgref_cdf_t80b_tv;
-class rhsgref_cdf_b_t80b_tv: rhsgref_cdf_t80b_tv
+class rhsgref_cdf_b_t80b_tv:rhsgref_cdf_t80b_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t80b_tv_base: rhsgref_cdf_b_t80b_tv
+class a3a_rhsgref_cdf_b_t80b_tv_base:rhsgref_cdf_b_t80b_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t80b_tv:a3a_rhsgref_cdf_b_t80b_tv_base
 {
@@ -463,17 +463,17 @@ class a3a_rhsgref_cdf_b_t80b_tv:a3a_rhsgref_cdf_b_t80b_tv_base
 };
 
 class rhsgref_cdf_t80bv_tv;
-class rhsgref_cdf_b_t80bv_tv: rhsgref_cdf_t80bv_tv
+class rhsgref_cdf_b_t80bv_tv:rhsgref_cdf_t80bv_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t80bv_tv_base: rhsgref_cdf_b_t80bv_tv
+class a3a_rhsgref_cdf_b_t80bv_tv_base:rhsgref_cdf_b_t80bv_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t80bv_tv:a3a_rhsgref_cdf_b_t80bv_tv_base
 {
@@ -488,17 +488,17 @@ class a3a_rhsgref_cdf_b_t80bv_tv:a3a_rhsgref_cdf_b_t80bv_tv_base
 };
 
 class rhsgref_cdf_t80u_tv;
-class rhsgref_cdf_b_t80u_tv: rhsgref_cdf_t80u_tv
+class rhsgref_cdf_b_t80u_tv:rhsgref_cdf_t80u_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t80u_tv_base: rhsgref_cdf_b_t80u_tv
+class a3a_rhsgref_cdf_b_t80u_tv_base:rhsgref_cdf_b_t80u_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t80u_tv:a3a_rhsgref_cdf_b_t80u_tv_base
 {
@@ -513,17 +513,17 @@ class a3a_rhsgref_cdf_b_t80u_tv:a3a_rhsgref_cdf_b_t80u_tv_base
 };
 
 class rhsgref_cdf_t80uk_tv;
-class rhsgref_cdf_b_t80uk_tv: rhsgref_cdf_t80uk_tv
+class rhsgref_cdf_b_t80uk_tv:rhsgref_cdf_t80uk_tv
 {
     class Turrets;
 };
-class a3a_rhsgref_cdf_b_t80uk_tv_base: rhsgref_cdf_b_t80uk_tv
+class a3a_rhsgref_cdf_b_t80uk_tv_base:rhsgref_cdf_b_t80uk_tv
 { 
     scope = 0; 
     class Turrets : Turrets 
-    { 
+    {
         class MainTurret; 
-    }; 
+    };
 };
 class a3a_rhsgref_cdf_b_t80uk_tv:a3a_rhsgref_cdf_b_t80uk_tv_base
 {
