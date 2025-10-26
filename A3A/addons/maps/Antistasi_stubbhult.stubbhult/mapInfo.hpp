1dab53804dc1c58703c084ec90eb80609fa6a976
Vlash <tate.chris271@gmail.com>
2025-10-26 10:59:15 +0000

    Base Files for Antistasi Stubbhult map

---
+++ A3A/addons/maps/Antistasi_stubbhult.stubbhult/mapInfo.hpp
@@ -0,0 +1,30 @@
+class stubbhult {
+    population[] = {
+        {"gussbo",49},{"aker",48},{"bockshult",60},{"ekenas",99},{"bexet",72},{"hylte",134},{"yaberg",54},{"gullanabba",348},{"balshult",170},{"femsjo",91},{"hallaboke",51},{"saraboke",93},{"hokhult",23},{"boalt",135},{"lojenas",71},{"sodra_bokeberg",29},{"norra_bokeberg",30},{"backhasten",19},{"rangalsbo",55},{"rangelsnas",35},{"ekenaso",10},{"krakshult",55},{"kalvhult",93},{"gnosjo",46},{"bohult",26},{"hjalmshult",35},{"sofieslatt",36},{"mjalahult",44},{"vallshult",47},{"saxhult",14},{"unnaryd",21},{"yttre_mjala",58},{"dohult",25},{"eldshult",69},{"ostra_bokeberg",57},{"langaryd",64}
+    };
+    disabledTowns[] = {
+    };
+    antennas[] = {
+    };
+    antennasBlacklistIndex[] = {};
+    banks[] = {};
+    garrison[] = {
+        {},{"factory_3", "seaport_4", "resource_4", "outpost_3", "airport_4"},
+        {},{}
+    };
+    fuelStationTypes[] = {
+        "Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F",
+        "Jbad_Ind_FuelStation_Feed"
+    };
+    climate = "temperate";
+    buildObjects[] = {
+    // Large vanilla tropical structures
+    {"Land_BagBunker_01_large_green_F", 300}, {"Land_HBarrier_01_tower_green_F", 300}, {"Land_BagBunker_01_small_green_F", 60},
+    {"Land_Shed_09_F", 120}, {"Land_Shed_10_F", 140},
+    // vanilla tropical sandbag walls
+    {"Land_BagFence_01_short_green_F", 10}, {"Land_BagFence_01_round_green_F", 10}, {"Land_BagFence_01_long_green_F", 10},
+    // Non-camo vanilla stuff
+    {"Land_SandbagBarricade_01_half_F", 20}, {"Land_SlumWall_01_s_2m_F", 5},
+    {"Land_Barricade_01_4m_F", 30}, {"Land_GuardBox_01_brown_F", 80}, {"Land_Tyres_F", 10}
+};
+};
