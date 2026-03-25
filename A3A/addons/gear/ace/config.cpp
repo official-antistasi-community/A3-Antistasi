dae0a5cbea4e45d286210ae0ca77d7cf53611651
ante <49555217+ante185@users.noreply.github.com>
2026-03-25 05:02:49 +0100

    config update, coffee & soda working

--- A3A/addons/gear/ace/config.cpp
+++ A3A/addons/gear/ace/config.cpp
@@ -2,11 +2,13 @@
 
 class CfgPatches 
 {
-    class ADDON {
+    class A3A_gear_ace_medical  {
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = 
 		{
+            "A3A_gear",
+            "A3_Data_F_AoW_Loadorder",
             "ace_medical_treatment"
 		};
         author = AUTHOR;
@@ -35,7 +37,7 @@ class CfgWeapons {
         descriptionUse = $STR_A3A_ESPRESSO_USE;
         class ItemInfo : ItemInfo
         {
-            mass = 10;
+            mass = 12;
         };
     };
 
@@ -46,7 +48,7 @@ class CfgWeapons {
         hiddenSelectionsTextures[] = {QPATHTOFOLDER(ace\assets\EsspInfBag-500ml.paa)};
         class ItemInfo : ItemInfo
         {
-            mass = 5;
+            mass = 6;
         };
     };
 
@@ -58,7 +60,7 @@ class CfgWeapons {
         hiddenSelectionsTextures[] = {QPATHTOFOLDER(ace\assets\EsspInfBag-250ml.paa)};
         class ItemInfo : ItemInfo
         {
-            mass = 2.5;
+            mass = 3;
         };
     };
 };
@@ -66,9 +68,10 @@ class CfgWeapons {
 class ace_medical_treatment {
     class Medication {
         class ACE_Can_Spirit {
+            painReduce = 0.035;
             dose = 1;
             maxDose = 40;
-            maxDoseDeviation = 2;
+            maxDoseDeviation = 12;
             timeInSystem = 3600;
             timeTillMaxEffect = 120;
             hrIncreaseLow[] = {0, 5};
@@ -78,6 +81,7 @@ class ace_medical_treatment {
         class ACE_Can_Franta : ACE_Can_Spirit {};
         class ACE_Can_RedGull : ACE_Can_Spirit {
             maxDose = 10;
+            timeInSystem = 7200;
             hrIncreaseLow[] = {5, 15};
             hrIncreaseNormal[] = {5, 15};
             hrIncreaseHigh[] = {5, 15};
@@ -87,20 +91,28 @@ class ace_medical_treatment {
             maxDose = 4;
             maxDoseDeviation = 2;
             timeInSystem = 3600;
-            timeTillMaxEffect = 10;
+            timeTillMaxEffect = 160;
             hrIncreaseLow[] = {10, 20};
             hrIncreaseNormal[] = {10, 50};
             hrIncreaseHigh[] = {10, 40};
         }
         class a3a_coffeeIV_500 : a3a_coffeeIV {
             dose = 0.5;
+            timeTillMaxEffect = 80;
             timeInSystem = 1800;
             volume = 500;
+            hrIncreaseLow[] = {5, 10};
+            hrIncreaseNormal[] = {5, 25};
+            hrIncreaseHigh[] = {5, 20};
         }
         class a3a_coffeeIV_250 : a3a_coffeeIV {
             dose = 0.25;
+            timeTillMaxEffect = 40;
             timeInSystem = 900;
             volume = 250;
+            hrIncreaseLow[] = {2.5, 5};
+            hrIncreaseNormal[] = {2.5, 12.5};
+            hrIncreaseHigh[] = {2.5, 10};
         }
     }
     class IV {
@@ -136,28 +148,30 @@ class ace_medical_treatment_actions {
             displayName = $STR_A3A_ESPRESSO_250_GIVE;
             items[] = {"a3a_coffeeIV_250"};
         }
-        class Painkillers;
-        class ACE_Can_Spirit : Painkillers{
+        class FieldDressing ;
+        class ACE_Can_Spirit : FieldDressing {
+            allowedSelections[] = {"Head"};
             condition = "params ['_medic', '_patient']; _medic == _patient;";
             displayName = "Drink can of Spirit";
             displayNameProgress = "Drinking";
             items[] = {"ACE_Can_Spirit"};
             animationMedic = "";
             animationMedicProne = "";
-            animationMedicSelf = ace_field_rations_drinkCrouchCan;
-            animationMedicSelfProne = ace_field_rations_drinkProneCan;
-            sounds[] = {"ace_field_rations_drinkCan1", "ace_field_rations_drinkCan1", "ace_field_rations_drinkCan2"};
-            litter[] = {{"ACE_Can_Spirit_Item"}};
+            animationMedicSelf = ""; 
+            animationMedicSelfProne = "";
+            litter[] = {{"Land_Can_V1_F"},{"Land_Can_Dented_F"},{"Land_Can_Dented_F"}};
+            callbackStart = "A3A_fnc_drinkMed";
+            treatmentTime = 10;
         };
         class ACE_Can_Franta : ACE_Can_Spirit {            
             displayName = "Drink can of Franta";
             items[] = {"ACE_Can_Franta"};
-            litter[] = {{"ACE_Can_Franta_Item"}};
+            litter[] = {{"Land_Can_V2_F"},{"Land_Can_V2_F"},{"Land_Can_V2_F"}};
         };
         class ACE_Can_RedGull : ACE_Can_Spirit {
             displayName = "Drink can of RedGull";
             items[] = {"ACE_Can_RedGull"};
-            litter[] = {{"ACE_Can_RedGull_Item"}};
+            litter[] = {{"Land_Can_V3_F"},{"Land_Can_V3_F"},{"Land_Can_V3_F"}};
         };
 }
 
@@ -168,6 +182,7 @@ class CfgFunctions
         class medical {
             file = "x\A3A\addons\gear\ace\functions";
             class coffeeIV {};
+            class drinkMed {};
         };
     };
 };
--- A3A/addons/gear/ace/functions/fn_coffeeIV.sqf
+++ A3A/addons/gear/ace/functions/fn_coffeeIV.sqf
@@ -1,5 +1,5 @@
 #include "..\..\script_component.hpp"
 params ["_medic", "_patient", "_bodyPart", "_classname", "_itemUser", "_usedItem"];
 
+["ace_medical_treatment_medicationLocal", [_patient, _bodyPart, _classname], _patient] call CBA_fnc_targetEvent;
 [_medic, _patient, _bodyPart, _classname, _itemUser, _usedItem] call ace_medical_treatment_fnc_ivBag;
-[_medic, _patient, _bodyPart, _classname, _itemUser, _usedItem] call ace_medical_treatment_fnc_medication;
