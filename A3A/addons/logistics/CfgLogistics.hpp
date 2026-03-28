aceeaba49261bd22029d0370833ce1e4d58291f6
skillless402 <Skillless402@gmail.com>
2026-03-28 17:02:48 -0400

    VME PLA mod addition
    adds the vme pla mod to the templates

--- A3A/addons/logistics/CfgLogistics.hpp
+++ A3A/addons/logistics/CfgLogistics.hpp
@@ -30,6 +30,7 @@ class DOUBLES(ADDON,Nodes)
     #include "Nodes\Vanilla.hpp"
     #include "Nodes\VN.hpp"
     #include "Nodes\WS.hpp"
+    #include "Nodes\VME.hpp"
 };
 
 class DOUBLES(ADDON,Cargo)
