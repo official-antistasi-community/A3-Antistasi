46021e49f863b34834f3c85eb2c39474dbff5814
ante <49555217+ante185@users.noreply.github.com>
2026-06-13 15:30:34 +0200

    M36 Variants

--- A3A/addons/config_fixes/MWB_M24/MWB_M36/config.cpp
+++ A3A/addons/config_fixes/MWB_M24/MWB_M36/config.cpp
@@ -22,8 +22,42 @@ class CfgPatches
 
     
 class CfgVehicles {
-    class MWB_M36B1;
-    class MWB_M36B2;
-    class a3a_MWB_M36B1_French : MWB_M36B1{};
-    class a3a_MWB_M36B2_French : MWB_M36B2{};
+    class SPE_M10;
+    class SPE_M4A1_75;
+    class MWB_M36B1 : SPE_M4A1_75 {
+        class EventHandlers;
+    };
+    class MWB_M36B2 : SPE_M10 {
+        class EventHandlers;
+};
+    class MWB_M36 : MWB_M36B2{
+        class EventHandlers;
+};
+    class a3a_MWB_M36_US : MWB_M36{
+        class EventHandlers : EventHandlers {
+            postinit = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        };
+        textureList[] = {"US_607TD_1945",1,"US_703TD_1945",1,"US_808TD_1945",1,"US_776TD_1945",1};
+    };
+    class a3a_MWB_M36B1_French : MWB_M36B1{
+        faction = "SPE_FR_ARMY";
+        class EventHandlers : EventHandlers {
+            postinit = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        };
+        textureList[] = {"France",1};
+    };
+    class a3a_MWB_M36B1_US : a3a_MWB_M36B1_French{
+        textureList[] = {"US_654TD_1945",1,"US_813TD_1945",1,"US_899TD_1945",1};
+    };
+    class a3a_MWB_M36B2_French : MWB_M36B2{
+        faction = "SPE_FR_ARMY";
+        class EventHandlers : EventHandlers {
+            postinit = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        };
+        textureList[] = {"French",1,"France2",1};
+    };
+    class a3a_MWB_M36B2_Korea : a3a_MWB_M36B2_French{
+        faction = "SPE_US_ARMY";
+        textureList[] = {"Korea",1};
+    };
 };
